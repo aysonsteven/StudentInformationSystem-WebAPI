102d5bf2c3327cbe58e1e89142fad647f1596c05
Mark Steven A. Nonato <aysonsteven@gmail.com>
2019-04-09 13:20:26 +0800

    initialize project

---
+++ src/main/java/io/project/webapi/StudentInfoSystemWebApiApplication.java
@@ -0,0 +1,13 @@
+package io.project.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInfoSystemWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInfoSystemWebApiApplication.class, args);
+	}
+
+}
